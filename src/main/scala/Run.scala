29e877a5fb8b0be13873fda8e8ec5df9ede50edc
amndzdzdz <zorrofx@gmx.net>
2025-09-20 14:12:56 +0200

    changed run file to use abstract syntax

--- src/main/scala/Run.scala
+++ src/main/scala/Run.scala
@@ -1,21 +1,26 @@
 package scala 
 
-import worldLogic.classes.Program
+import worldLogic.classes.GameWorld
+import scala.dsl.Program
+import scala.dsl.moveForward
+import scala.dsl.TurnRight
+import scala.dsl.PickUp
+import scala.dsl.Drop
+import dsl.Interpreter.execute
 
 object Run {
     //example interaction
     @main def main(): Unit = {
-        val world = Program(worldSize = 5)
-        
-        world.visualzeStart()
-        world.move(3)
-        world.turnRight()
-        world.move(4)
-        world.tryPickUp()
-        world.turnLeft()
-        world.move(4)
-        world.tryDrop()
-        world.turnRight()
-        world.move(4)
+        val world = GameWorld(worldSize = 5)
+        val exampleProgram = new Program(List(
+            new moveForward(2),
+            new TurnRight(),
+            new moveForward(2),
+            new PickUp(),
+            new moveForward(1),
+            new Drop()
+        ))
+
+        execute(exampleProgram, world)
     }
 }
