3c26c8838fb00365a8e6ec42c6eb1d57b97ca67f
amndzdzdz <zorrofx@gmx.net>
2025-09-21 13:45:20 +0200

    changed main file to read  grammar from file and execute commands

--- src/main/scala/Run.scala
+++ src/main/scala/Run.scala
@@ -1,23 +1,28 @@
-package scala 
+package scala
 
 import worldLogic.classes.GameWorld
 import scala.dsl.Program
 import scala.dsl.Command
 import dsl.Interpreter.execute
+import scala.dsl.Interpreter.runProgram
+import scala.dsl.Parser
+
+
+def readFile(file: String) = {
+    scala.io.Source.fromFile(file).getLines.mkString("\n")
+}
 
 object Run {
+    def readFile(file: String) = {
+        scala.io.Source.fromFile(file).getLines.mkString("\n")
+    }
+
     //example interaction
     @main def main(): Unit = {
         val world = GameWorld(worldSize = 5)
-        val exampleProgram = new Program(List(
-            Command.Move(2),
-            Command.TurnRight(),
-            Command.Move(2),
-            Command.PickUp(),
-            Command.Move(1),
-            Command.Drop()
-        ))
-
-        execute(exampleProgram, world)
+        val parser = new Parser()
+        val file = readFile("show.niki")
+        val result = parser.parseProgram(file)
+        runProgram(result, world)
     }
 }
