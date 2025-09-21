940c377253ae59656c7f3284123645e162748147
amndzdzdz <zorrofx@gmx.net>
2025-09-21 08:47:38 +0200

    extended the abstract syntax with conditions and additional commands

--- src/main/scala/dsl/abstractSyntax.scala
+++ src/main/scala/dsl/abstractSyntax.scala
@@ -5,7 +5,22 @@ case class Program(commands: List[Command])
 enum Command {
     case TurnLeft()
     case TurnRight()
-    case MoveForward(steps: Int)
+    case Move(steps: Int)
     case PickUp()
     case Drop()
+    case ShowIf(condition: Condition)
+    case IfCondition(condition: Condition, program: Program)
+    case WhileCondition(condition: Condition, program: Program)
+}   
+
+enum Condition {
+    case True()
+    case SquareHasItems()
+    case RobotHasItems()
+    case IsRockAhead()
+    case IsEdgeAhead()
+    case NotCondition(condition: Condition)
+    case AndConditions(leftCondition: Condition, rightCondition: Condition)
+    case OrConditions(leftCondition: Condition, rightCondition: Condition)
+    case BracketCondition(condition: Condition)
 }
