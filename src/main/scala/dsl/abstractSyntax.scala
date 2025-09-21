901815ad01ffbae818acc28d0b645d27c3bfde8c
amndzdzdz <zorrofx@gmx.net>
2025-09-21 13:42:49 +0200

    removed accidentally added brackets in abstract syntax

--- src/main/scala/dsl/abstractSyntax.scala
+++ src/main/scala/dsl/abstractSyntax.scala
@@ -3,24 +3,24 @@ package scala.dsl
 case class Program(commands: List[Command])
 
 enum Command {
-    case TurnLeft()
-    case TurnRight()
+    case TurnLeft
+    case TurnRight
     case Move(steps: Int)
-    case PickUp()
-    case Drop()
+    case PickUp
+    case Drop
     case ShowIf(condition: Condition)
-    case IfCondition(condition: Condition, program: Program)
-    case WhileCondition(condition: Condition, program: Program)
+    case IfCommand(condition: Condition, left: Program, right: Program)
+    case WhileCommand(condition: Condition, program: Program)
 }   
 
 enum Condition {
-    case True()
-    case SquareHasItems()
-    case RobotHasItems()
-    case IsRockAhead()
-    case IsEdgeAhead()
+    case True
+    case False
+    case SquareHasItems
+    case RobotHasItems
+    case IsRockAhead
+    case IsEdgeAhead
     case NotCondition(condition: Condition)
     case AndConditions(leftCondition: Condition, rightCondition: Condition)
     case OrConditions(leftCondition: Condition, rightCondition: Condition)
-    case BracketCondition(condition: Condition)
 }
